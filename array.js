980bcf522b3a7e4899b2aecdb9799561d6fc04b9
Aryan Panchal <panchalaryan946@gmail.com>
2026-09-19 12:04:08 +0530

    js

---
+++ array.js
@@ -0,0 +1,24 @@
+//used to store same (hetrogenes) elements
+//store differnt type of elements
+//mutable : og array can be change
+
+let arr=[1,2,3,4,"Aryan",true];
+console.log(arr.length);
+
+//for iteration
+//1.for loop:
+
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+//2.for of loop:
+
+for(let a of arr)
+{
+    console.log(arr[i]);
+}
+
+//3.for eachn loop
+
+for
