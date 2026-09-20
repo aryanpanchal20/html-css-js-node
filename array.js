1fb35fb250b133d7e428fe528dad43bb8047f842
Aryan Panchal <panchalaryan946@gmail.com>
2026-09-20 12:56:47 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,54 +1,65 @@
-//used to store same (hetrogenes) elements
-//store differnt type of elements
-//mutable : og array can be change
+// //used to store same (hetrogenes) elements
+// //store differnt type of elements
+// //mutable : og array can be change
 
-let arr=[1,2,3,4,"Aryan",true];
-console.log(arr.length);
+// let arr=[1,2,3,4,"Aryan",true];
+// console.log(arr.length);
 
-console.log("=========================================================================");
-
-
-//for iteration
-//1.for loop:
-
-for(let i=0;i<arr.length;i++)
-{
-    console.log(arr[i]);
-}
-console.log("=========================================================================");
-
-//2.for of loop:
-
-for(let a of arr)
-{
-    console.log(a);
-}
-
-console.log("=========================================================================");
-//3.for eachn loop -->functional  programming -->inbuilt functions
-
-//callback:func sending another func as argument
-//highorder:func receive another func as argument
-
-arr.forEach((val)=>{console.log(val);})
+// console.log("=========================================================================");
 
 
-//1.for each function : to iterate array
-let narr=[1,2,3,4,5];
+// //for iteration
+// //1.for loop:
 
-narr.forEach((val,index,ar)=>{console.log(val,index,ar);})
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+// console.log("=========================================================================");
 
- console.log("=========================================================================");
- //2.map function:original array refernece  --generate new array
+// //2.for of loop:
 
- let newarr=narr.map(val=>val*3)
+// for(let a of arr)
+// {
+//     console.log(a);
+// }
 
- console.log(newarr);
+// console.log("=========================================================================");
+// //3.for eachn loop -->functional  programming -->inbuilt functions
 
- console.log("=========================================================================");
+// //callback:func sending another func as argument
+// //highorder:func receive another func as argument
 
- //3.filter function:to filter function based on condition
+// arr.forEach((val)=>{console.log(val);})
 
- let oddarr=narr.filter(val=>val%2!=0);
 
- console.log(oddarr);
+// //1.for each function : to iterate array
+// let narr=[1,2,3,4,5];
+
+// narr.forEach((val,index,ar)=>{console.log(val,index,ar);})
+
+//  console.log("=========================================================================");
+//  //2.map function:original array refernece  --generate new array
+
+//  let newarr=narr.map(val=>val*3)
+
+//  console.log(newarr);
+
+//  console.log("=========================================================================");
+
+//  //3.filter function:to filter function based on condition
+
+//  let oddarr=narr.filter(val=>val%2!=0);
+
+//  console.log(oddarr);
+
+//  console.log("=========================================================================");
+
+//  //reduce function():reduces array to generate single value
+
+//  let sum=narr.reduce((currentval,nextval)=>{return currentval*nextval});
+//  console.log(sum);
+
+let arr1=[1,2,3,4,5];
+
+arr1.forEach((val)=>{console.log(val);})
